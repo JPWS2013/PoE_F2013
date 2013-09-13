104caa8c1e79b839ea455c20b7581a7ab3b4b87e
Justin <justin.poh@students.olin.edu>
2013-09-13 10:10:38 -0400

    Updated code to clean up the arduino output and store it in a list object

--- Lab_1/main_python_code.py
+++ Lab_1/main_python_code.py
@@ -1,6 +1,23 @@
 import serial
 
-ser=serial.Serial('/dev/tty', 9600)
+ser=serial.Serial('/dev/ttyACM0', 9600) #Defines the serial port to use 
 
-while True:
-print ser.readline()
+data=[] #List object that stores the data from the arduino
+counter=0 #Counter that counts how many data points it has taken (May not be needed for final code)
+
+while True: #While able to get data?
+	while counter!=30: #Until it has 30 data points
+		reading= ser.readline() #Read the output from the arduino through the serial port
+
+		if counter != 0: #Ignores the very first reading from the arduino becuase that is usually a junk reading
+			cleanReading=reading[0:-2] #Removes the last 2 characters ("\r\n") from the arduino output
+			cleanReading=int(cleanReading) #Type-casts it as an integer so that it can be plotted
+			data.append(cleanReading) #Appends it to the data list
+		
+		counter += 1 #Increments the counter
+
+		print counter #This is here just so that there is an output to tell the user how many data points have been stored
+
+	break #Once it's done getting the data, break the while loop
+
+print data #Display the data on screen
