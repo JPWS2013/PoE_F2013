1d95fb90e9e8d4d6ec51889c5f199cc858ea2257
Justin <justin.poh@students.olin.edu>
2013-09-17 22:40:55 -0400

    Added lidar class definition file for the lidar object

---
+++ Lab_1/lidar_classdef.py
@@ -0,0 +1,16 @@
+import math
+class lidar:
+	def __init__(self, input_horz_ang=0, input_vert_ang=0, radius=0):
+		horz_ang=math.radians(input_horz_ang)
+		vert_ang=math.radians(input_vert_ang)
+		r=radius
+		
+		self.z_pos = r*math.sin(vert_ang)
+		self.y_pos = r*math.cos(vert_ang)*math.cos(horz_ang)
+		self.x_pos = r*math.cos(vert_ang)*math.sin(horz_ang)
+
+	def display_data(self):
+
+	 	print "X Position = ", self.x_pos
+		print "Y Position = ", self.y_pos
+		print "Z Position = ", self.z_pos
