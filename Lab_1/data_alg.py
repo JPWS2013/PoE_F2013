6be22a131d89caeb5570b110145305ba4fbcf783
Justin <justin.poh@students.olin.edu>
2013-09-22 19:34:48 -0400

    Added documentation to the python code

--- Lab_1/data_alg.py
+++ Lab_1/data_alg.py
@@ -8,16 +8,15 @@ from lidar_classdef import *
 ser=serial.Serial('/dev/ttyACM0', 9600) #Defines the serial port to use 
 
 data=[] #List that stores the data from the arduino
-counter=0 #Counter that counts how many data points it has taken (May not be needed for final code)
 
-horizontal_angle_track=130 #Keeps track of the horizontal angle that has already been swept by the arduino
-vertical_angle_track=0 #Keeps track of the vertical angle that has already been swept by the arduino
+horizontal_angle_track=130 #Keeps track of the horizontal angular position of the sensor
+vertical_angle_track=0 #Keeps track of the vertical angular position of the sensor
 
 print "Waiting for arduino to be ready....." #Indicates that the programme is waiting for the arduino to initialize
 
 time.sleep(1) #Waits for the arduino to initialize
 
-print "Programme beginning now" #Indicates that the arduino is running its programme
+print "Programme beginning now" #Indicates that the python programme is running
 
 #Moves the servo to the starting position
 
@@ -27,10 +26,10 @@ time.sleep(0.5) #Waits half a second to ensure the arduino is ready for next com
 ser.write('6') #Sets the servo to it's starting vertical angle
 vertical_angle_track=90 #Sets the angle to the vertical_angle_track angle
 
-horz_angle=range(horizontal_angle_track, 45, -1) #
-vert_angle=range(vertical_angle_track,vertical_angle_track+1,1)
+horz_angle=range(horizontal_angle_track, 45, -1) #creates the horizontal range of angles that the servo should sweep through
+vert_angle=range(vertical_angle_track,vertical_angle_track+1,1) #Creates the vertical range of angles that the servo should sweep through
 
-for eachVerticalAngle in vert_angle:	
+for eachVerticalAngle in vert_angle:
 	#print "I'm sending code to the arduino to reset it back all the way to the left"
 	#time.sleep(1)
 	#ser.write('1')
@@ -43,42 +42,38 @@ for eachVerticalAngle in vert_angle:
 		#print "I'm sending the code to the arduino to turn the servo 1 degree to the right"
 		#print "Degree = ", eachHorizontalAngle
 		#time.sleep(1)
-		ser.write('7')
+		ser.write('7') #Tells the arduino to send back a distance reading
 
-		distance_response=ser.readline()
+		distance_response=ser.readline() #Receives the distance reading from the arduino
 		cleanReading=distance_response[0:-2] #Removes the last 2 characters ("\r\n") from the arduino output
-		distance=float(cleanReading) #Type-casts it as an integer so that it can be plotted
-		print "distance= ", distance
+		distance=float(cleanReading) #Type-casts it as an float so that it can be plotted
+		#print "distance= ", distance
 
-		ser.write('8')
+		ser.write('8') #Tells the arduino to send back the current vertical and horizontal angles
 
-		angle_response=ser.readline()
+		angle_response=ser.readline() #Receives the vertical and horizontal angles from the arduino as a string
 
 		cleanReading=angle_response[0:-2] #Removes the last 2 characters ("\r\n") from the arduino output
-		seperator_index=cleanReading.index(',')
-		vertical_angle=cleanReading[0:seperator_index]
-		vertical_angle=int(vertical_angle)
-		print "vertical_angle= ", vertical_angle
-		horizontal_angle=cleanReading[(seperator_index+1): ]
-		horizontal_angle=int(horizontal_angle)
-		print "horizontal_angle= ", horizontal_angle
+		seperator_index=cleanReading.index(',') #Figures out at what index the comma appears (which is how we delineate vertical and horizontal angle)
+		vertical_angle=cleanReading[0:seperator_index] #Retreives the vertical angle from the string that is received from the arduino
+		vertical_angle=int(vertical_angle) #Converts the angle from string to integer
+		#print "vertical_angle= ", vertical_angle 
+		horizontal_angle=cleanReading[(seperator_index+1): ] #Retreives the horizontal angle from the string received from the arduino
+		horizontal_angle=int(horizontal_angle) #Converts the angle from a string to a integer
+		#print "horizontal_angle= ", horizontal_angle
 
-		processed_full_data_holder=lidar(horizontal_angle, vertical_angle, distance)
+		processed_full_data_holder=lidar(horizontal_angle, vertical_angle, distance) #Passes the distance reading and angles into a lidar object (defined seperately)
 
-		data.append(processed_full_data_holder)
+		data.append(processed_full_data_holder) #Appends the new lidar object to the data list
 
-		ser.write('1')
-		horizontal_angle_track=horizontal_angle_track-2
+		ser.write('1') #Moves the servo one degree to the right after taking a reading
 		#print 'horizontal_angle_track= ', horizontal_angle_track
-		#time.sleep(0.5)
 
-	ser.write('3')
-	vertical_angle_track=vertical_angle_track+3
+	ser.write('3') #At the end of the horizontal sweep, increase the vertical angle by 1 degree
 	#print 'direction_track= ', direction_track
 	time.sleep(0.5)
-
-	ser.write('5')
-	horizontal_angle_track=140
+ 
+	ser.write('5') #Resets the arduino back to the left to perform a full horizontal sweep 
 	time.sleep(0.5)
 
 for eachData in data:
@@ -88,8 +83,8 @@ for eachData in data:
 #def randrange(n, vmin, vmax):
 #    return (vmax-vmin)*np.random.rand(n) + vmin
 
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
+fig = plt.figure() #Creates a new pyplot figure
+ax = fig.add_subplot(111, projection='3d') #adds a 3d plot axis to it
 # '''n = 100
 # for c, m, zl, zh in [('r', 'o', -50, -25), ('b', '^', -30, -5)]:
 #     xs = randrange(n, 23, 32)
@@ -97,19 +92,19 @@ ax = fig.add_subplot(111, projection='3d')
 #     zs = randrange(n, zl, zh)
 #     ax.scatter(xs, ys, zs, c=c, marker=m)'''
 
-xs = []
-ys = []
-zs = []
+xs = [] #Creates a new list for x axis values
+ys = [] #Creates a new list for y axis values
+zs = [] #Creates a new list for z axis values
 
 for eachPoint in data:
-	xs.append(eachPoint.x_pos)
-	ys.append(eachPoint.y_pos)
-	zs.append(eachPoint.z_pos)
+	xs.append(eachPoint.x_pos) #Adds each x axis value to the x axis data list
+	ys.append(eachPoint.y_pos) #Adds each y axis value to the y axis data list
+	zs.append(eachPoint.z_pos) #Adds each z axis value to the z axis data list
 
-ax.scatter(xs,ys,zs, c ='r', marker = 'o')
+ax.scatter(xs,ys,zs, c ='r', marker = 'o') #Creates a scatter plot of all data
 
-ax.set_xlabel('X Label')
-ax.set_ylabel('Y Label')
-ax.set_zlabel('Z Label')
+ax.set_xlabel('X Label') #X axis label
+ax.set_ylabel('Y Label') #Y axis label
+ax.set_zlabel('Z Label') #Z axis label
 
-plt.show()
+plt.show() #show the pyplot figure
--- Lab_1/lidar_classdef.py
+++ Lab_1/lidar_classdef.py
@@ -1,15 +1,15 @@
-import math
-class lidar:
-	def __init__(self, input_horz_ang=0, input_vert_ang=0, radius=0):
-		horz_ang=math.radians(input_horz_ang)
-		vert_ang=math.radians(input_vert_ang)
-		r=radius
-		
-		self.z_pos = -r*math.cos(vert_ang)
-		self.y_pos = r*math.sin(vert_ang)*math.sin(horz_ang)
-		self.x_pos = r*math.sin(vert_ang)*math.cos(horz_ang)
+import math #Imports math to do trig calculations
 
-	def display_data(self):
+class lidar: #Definition of a new class called lidar
+	def __init__(self, input_horz_ang=0, input_vert_ang=0, radius=0):
+		horz_ang=math.radians(input_horz_ang) #converts the horizontal angle from degrees to radians
+		vert_ang=math.radians(input_vert_ang) #converts the vertical angle from degrees to radians
+		
+		self.z_pos = -radius*math.cos(vert_ang) #Calculates the length of the position vector projected on the z-axis
+		self.y_pos = radius*math.sin(vert_ang)*math.sin(horz_ang) #Calculates the length of the position vector projected onto the y axis
+		self.x_pos = radius*math.sin(vert_ang)*math.cos(horz_ang) #Calculates the length of the position vector projected onto the x axis
+
+	def display_data(self): #Instance method to display the data encapsulated in the lidar object
 
 	 	print "X Position = ", self.x_pos
 		print "Y Position = ", self.y_pos
